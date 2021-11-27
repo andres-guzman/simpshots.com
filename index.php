c6ea22bbdf979c615bbb7293e0580668932810e7
andres-guzman <andres.16602@gmail.com>
2021-11-27 13:19:37 -0400

    removed prevent default
    not doing any good

--- index.php
+++ index.php
@@ -104,7 +104,6 @@ $img = getRandomFromArray($imgList);
                         xhr.send();
                     });
                 }
-                evt.preventDefault();
             });
         </script>
 
